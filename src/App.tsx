e1e393ffacab3eb360d18851cdffe3e8154c0a17
contigen <69436315+contigen@users.noreply.github.com>
2024-03-10 20:02:46 +0100

    Remove  unused state var

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import { Shake } from './shake'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <h1>Shake to Refresh using the Device Motion Event.</h1>
