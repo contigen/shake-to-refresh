50b3ef215224589bb43630252fbfa5f99d3e8af8
contigen <69436315+contigen@users.noreply.github.com>
2024-03-11 05:30:46 +0100

    Reduce accelerartion magnitude

--- src/shake.tsx
+++ src/shake.tsx
@@ -36,7 +36,7 @@ export function Shake() {
         const { x, y, z } = acceleration
         const accelerationMagnitude = Math.sqrt(x ** 2 + y ** 2 + z ** 2)
 
-        if (accelerationMagnitude >= 15) {
+        if (accelerationMagnitude >= 5) {
           reload()
         }
       },
